932a53592a840ea568bbd5fc3d5a6cc18d6d6804
Ian Gluesing <ianmsgames@gmail.com>
2025-05-10 11:06:09 -0500

    Add utility to get image points more efficiently from video stream

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 cam_stream.cpp -o cam_stream `pkg-config --cflags --libs opencv4` -std=c++11
+
+ffmpeg -ss 00:00:00.000 -i example_recording.mov -pix_fmt rgb24 -r 10 -s 320x240 -t 00:01:00.000 output.gif
---
+++ app/camera_point_utility/main.cpp
@@ -0,0 +1,37 @@
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+void onMouse(int event, int x, int y, int, void*) {
+    if (event == cv::EVENT_MOUSEMOVE) {
+        std::cout << "Mouse Position: (" << x << ", " << y << ")" << std::endl;
+    }
+}
+
+int main() {
+
+    cv::VideoCapture cap("https://iowadotsfs1.us-east-1.skyvdn.com:443/rtplive/ictv03lb/playlist.m3u8"); 
+    if (!cap.isOpened()) {
+        std::cerr << "Error opening video stream" << std::endl;
+        return 0;
+    }
+
+    cv::Mat frame;
+    cap >> frame;
+    
+    cv::namedWindow("Image Window", cv::WINDOW_AUTOSIZE);
+
+    // Set mouse callback
+    cv::setMouseCallback("Image Window", onMouse, nullptr);
+
+    // Display loop
+    while (true) {
+        cv::imshow("Image Window", frame);
+
+        // Break on ESC key
+        if (cv::waitKey(1) == 27) break;
+    }
+
+    cv::destroyAllWindows();
+    return 0;
+}
